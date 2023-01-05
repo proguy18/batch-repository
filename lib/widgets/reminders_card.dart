61c8630b80b40e315ecbf9fa484d4a3bed08fc1b
proguy18 <josephjoey.leonardi@gmail.com>
2023-01-06 04:49:05 +0700

    Edited reminder_card UI

--- lib/widgets/reminders_card.dart
+++ lib/widgets/reminders_card.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:telecomm_mobile/glassmorphism/glassmorphism.dart';
 import 'package:flutter/material.dart';
 import 'package:telecomm_mobile/models/reminders_model.dart';
 import 'package:intl/intl.dart';
@@ -14,105 +15,130 @@ class RemindersCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // User user = FirebaseAuth.instance.currentUser!;
-    return Padding(
-        padding: const EdgeInsets.all(0.0),
-        child: Container(
-            height: MediaQuery.of(context).size.height * 0.2,
-            color: Colors.transparent,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.2,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
-                  color: Color.fromARGB(255, 187, 242, 70)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Date
-                  Container(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        DateFormat('dd/MM/yyyy, HH:mm')
-                            .format(reminders.reminderDate.toDate())
-                            .toString(),
-                        style: GoogleFonts.openSans(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-                  // Description
-                  Container(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        reminders.description,
-                        style: GoogleFonts.openSans(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        reminders.name,
-                        style: GoogleFonts.openSans(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child:
-                          //1
-                          IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.call),
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      DateFormat('dd/MM/yyyy, HH:mm')
+                          .format(reminders.reminderDate.toDate())
+                          .toString(),
+                      style: GoogleFonts.montserrat(
+                        fontSize: 20,
                         color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
-                      //2
-                      // Container(
-                      //   child: TextButton(
-                      //       onPressed: () async {
-                      //         url_launcher.launchUrl(
-                      //             Uri.parse(
-                      //                 'https://wa.me/${reminders.locationPhoneNumber.toString()}'),
-                      //             mode: url_launcher
-                      //                 .LaunchMode.externalApplication);
-                      //       },
-                      //       child: Text(
-                      //         '\+' + reminders.locationPhoneNumber.toString(),
-                      //         style: TextStyle(fontSize: 20, color: Colors.black),
-                      //       )),
-                      // ),
                     ),
-                  ),
-                  //3
-                  // TextButton(
-                  //   onPressed: () => showDialog(
-                  //     context: context,
-                  //     builder: (context) => DeleteReminders(
-                  //         remindersID: reminders.remindersID, uid: user.uid),
-                  //   ),
-                  //   child: Text(
-                  //     'Cancel Reminders',
-                  //     style: TextStyle(fontSize: 20, color: Colors.red),
-                  //   ),
-                  // ),
-                ],
+                    SizedBox(height: 8),
+                    Text(
+                      reminders.description,
+                      style: GoogleFonts.montserrat(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      reminders.name,
+                      style: GoogleFonts.montserrat(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    // Text(
+                    //   reminders.type,
+                    //   style: GoogleFonts.montserrat(
+                    //     fontSize: 16,
+                    //     color: Colors.black,
+                    //   ),
+                    // ),
+                    // SizedBox(height: 8),
+                    // Text(
+                    //   reminders.number.toString(),
+                    //   style: GoogleFonts.montserrat(
+                    //     fontSize: 16,
+                    //     color: Colors.black,
+                    //   ),
+                    // ),
+                    SizedBox(height: 8),
+                    IconButton(
+                      color: Colors.red,
+                      icon: Icon(Icons.phone),
+                      onPressed: () {
+                        // Add code here to initiate a phone call
+                      },
+                    ),
+                  ],
+                ),
               ),
-            )));
+            );
+          },
+        );
+      },
+      child: Card(
+        color: Colors.transparent,
+        child: GlassMorphism(
+          blur: 10,
+          opacity: 0.05,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  reminders.name,
+                  style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 75,
+                        height: 20,
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Text(
+                            DateFormat('HH:mm')
+                                .format(reminders.reminderDate.toDate())
+                                .toString(),
+                            style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontSize: 20,
+                              // fontWeight: FontWeight,
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        color: Colors.red,
+                        icon: Icon(Icons.phone),
+                        onPressed: () {
+                          // Add code here to initiate a phone call
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
